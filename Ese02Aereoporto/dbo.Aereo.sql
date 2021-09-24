2363f6bcfa40ee874e32f13400f36603be62be17
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-09-24 10:44:35 +0200

    Query Ese 02 Aeroporto

---
+++ Ese02Aereoporto/dbo.Aereo.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Aereo] (
+    [TipoAereo]     VARCHAR (10) NOT NULL,
+    [NumPasseggeri] INT          NULL,
+    [QtaMerci]      INT          NULL,
+    PRIMARY KEY CLUSTERED ([TipoAereo] ASC)
+);
+
+
+
+
---
+++ Ese02Aereoporto/query.sql
@@ -0,0 +1,20 @@
+﻿/*Le città da cui partono per Milano in ordine alfabetico*/
+SELECT DISTINCT v.CittaPartenza
+FROM Volo v
+WHERE v.CittaArrivo='Milano'
+ORDER BY v.CittaPartenza
+
+
+/* Città con aereoporto con numero di piste non noto */
+SELECT a.Citta
+FROM Aeroporto a 
+WHERE a.NumPiste IS NULL
+
+
+/* Per ogni volo Misto (sia merci che passeggeri): codice volo e dati di trasporto*/
+
+SELECT * 
+FROM Aereo a, Volo v
+WHERE a.TipoAereo= v.TipoAereo
+AND a.QtaMerci>0
+AND a.NumPasseggeri>0
