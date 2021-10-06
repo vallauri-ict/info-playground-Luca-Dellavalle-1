f20206473b52fc5578cb0a51e73a1321d115aa4c
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-10-06 20:07:38 +0200

    create table query e insert

--- Ese05sqlAuto/SQLQuery.sql
+++ Ese05sqlAuto/SQLQuery.sql
@@ -1,37 +1,61 @@
-﻿DROP TABLE Auto, Proprietari, Assicurazioni, Sinistro, Autocoinvolte;
+﻿DROP TABLE AutoCoinvolte,_Auto, Sinistro, Assicurazioni, Proprietari;
 
 CREATE TABLE Proprietari(
-	CodF int PRIMARY KEY,
+	CodF int PRIMARY KEY IDENTITY,
 	Nome varchar(50) NOT NULL,
 	Residenza varchar(40) NOT NULL
 )
 
 CREATE TABLE Assicurazioni(
-	CodAss int PRIMARY KEY,
+	CodAss int PRIMARY KEY IDENTITY,
 	Nome varchar(30) NOT NULL,
 	Sede varchar(40) NOT NULL
 )
 
-CREATE TABLE Auto(
+CREATE TABLE Sinistro(
+	CodS int PRIMARY KEY IDENTITY,
+	Localita varchar(40) NOT NULL,
+	DataS DATETIME NULL
+)
+
+CREATE TABLE _Auto(
 	Targa varchar(20) PRIMARY KEY,
 	Marca varchar(30) NOT NULL,
 	Cilindrata int NOT NULL ,
 	Potenza int NOT NULL,
-	FOREIGN KEY (CodF) REFERENCES  Proprietari(Codf),
-	FOREIGN KEY (CodAss) REFERENCES Assicurazioni(CodAss)
+	CodF int FOREIGN KEY REFERENCES  Proprietari(CodF),
+	CodAss int FOREIGN KEY REFERENCES Assicurazioni(CodAss)
 )
 
-CREATE TABLE Sinistro(
-	CodS int PRIMARY KEY,
-	Località varchar(40) NOT NULL,
-	DataS DATETIME NULL
-)
-
-CREATE TABLE Autocoinvolte(
-	FOREIGN KEY (CodS) REFERENCES Sinistro(CodS),
-	FOREIGN KEY (Targa) REFERENCES Auto(Targa),
+CREATE TABLE AutoCoinvolte(
+	CodS int FOREIGN KEY REFERENCES Sinistro(CodS),
+	Targa varchar(20) FOREIGN KEY REFERENCES _Auto(Targa),
 	ImportoDelDanno int NOT NULL
 )
 
+/*INSERT*/
 
+INSERT INTO Proprietari VALUES
+('Mario','Savona'),
+('Sergio','Bra'),
+('Sofia','Cuneo');
 
+INSERT INTO Assicurazioni VALUES
+('Vittoria','Roma'),
+('Cattolica','Torino'),
+('Unicredit','Milano');
+
+INSERT INTO Sinistro VALUES
+('Alba','20032021'),
+('Monchiero','21052021'),
+('Dogliani','05032021');
+
+INSERT INTO _Auto VALUES
+('AB990FD','Mercedes',100,50),
+('ER345TR','Audi',200,60),
+('RT432FR','BMW',300,70);
+
+INSERT INTO AutoCoinvolte VALUES
+(10000),
+(20000),
+(1000);
