d372a5afa3f9fbbbe453ce40cec22ce1ed22756c
Luca Dellavalle <l.dellavalle.1029@vallauri.edu>
2022-02-18 16:49:00 +0100

    Compito 11 StoreProcedure Classe

---
+++ Ese11StoredProcedureStudenti/Ese11StoredProcedureStudenti/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ese11StoredProcedureStudenti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnElencoStudenti = new System.Windows.Forms.Button();
+            this.txtClasse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnElencoStudenti
+            // 
+            this.btnElencoStudenti.Location = new System.Drawing.Point(44, 131);
+            this.btnElencoStudenti.Name = "btnElencoStudenti";
+            this.btnElencoStudenti.Size = new System.Drawing.Size(160, 56);
+            this.btnElencoStudenti.TabIndex = 0;
+            this.btnElencoStudenti.Text = "ELENCO STUDENTI";
+            this.btnElencoStudenti.UseVisualStyleBackColor = true;
+            this.btnElencoStudenti.Click += new System.EventHandler(this.btnElencoStudenti_Click);
+            // 
+            // txtClasse
+            // 
+            this.txtClasse.Location = new System.Drawing.Point(44, 93);
+            this.txtClasse.Name = "txtClasse";
+            this.txtClasse.Size = new System.Drawing.Size(100, 22);
+            this.txtClasse.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserimento classe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClasse);
+            this.Controls.Add(this.btnElencoStudenti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnElencoStudenti;
+        private System.Windows.Forms.TextBox txtClasse;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ese11StoredProcedureStudenti/Ese11StoredProcedureStudenti/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ese11StoredProcedureStudenti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\'C:\\Users\\Luca Dellavalle\\Documents\\GitHub\\info-playground-Luca-Dellavalle-1\\Ese11StoredProcedureStudenti\\Scuola.mdf\';Integrated Security = True; Connect Timeout = 30"; 
+
+        private void btnElencoStudenti_Click(object sender, EventArgs e)
+        {
+            string sql = "ElencoStudenti";
+
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    SqlParameter nomeBanca = new SqlParameter();
+                    nomeBanca.Value = txtClasse.Text;
+                    nomeBanca.ParameterName = "nomeBanca";
+                    nomeBanca.Direction = ParameterDirection.Input;
+                    nomeBanca.DbType = DbType.String;
+                    nomeBanca.Size = 50;
+                    cmd.Parameters.Add(nomeBanca);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader ris = cmd.ExecuteReader();
+
+                    while (ris.Read())
+                    {
+                        string s = "";
+                        for (int i = 0; i < ris.FieldCount; i++)
+                        {
+                            s += ris.GetValue(i) + " - ";
+                        }
+                        MessageBox.Show(s, "Risultato stored");
+                    }
+
+                    ris.Close();
+
+                    MessageBox.Show("");
+
+                }
+            }
+        }
+    }
+}
